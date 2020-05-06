545f11de491c66802ee7ada6d3153c6aec9a2419
xAirx <marcowurtz@hotmail.com>
2020-05-06 07:12:11 +0200

    Add copy to clipboard

--- colorpaletteproject/src/ColorBox.js
+++ colorpaletteproject/src/ColorBox.js
@@ -1,28 +1,37 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export class ColorBox extends Component {
   constructor(props) {
     super(props);
 
     this.state = {};
+    this.handleCopy = this.handleCopy.bind(this);
+  }
+
+  handleCopy() {
+    console.log('We copied something');
   }
 
   render() {
     const { name, background } = this.props;
 
     return (
+      <CopyToClipboard text="{this.props.background}">
       <div style={{ background }} className="ColorBox">
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
           </div>
-          <button type="button" className="copy-button">
+          <button type="button" className="copy-button" onClick={this.handleCopy}>
             Copy
           </button>
         </div>
         <span className="see-more">More</span>
       </div>
+      </CopyToClipboard>
     );
   }
 }
